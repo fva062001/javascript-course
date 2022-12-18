e89c0f08a97fb57f0cebbfce2b47d8be93a30a5a
Fernando Arturo Valerio <fernandoarturo06@gmail.com>
2022-12-18 15:33:53 -0400

    Updating

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -77,34 +77,150 @@
 // };
 
 // greet('Hey')('Fernando');
-const lufthansa = {
-  name: 'Lufthansa',
-  iataCode: 'LH',
-  bookings: [],
-  book(flightNum, name) {
-    console.log(
-      `${name} booked a seat on ${this.name} flight ${this.iataCode}${flightNum}`
-    );
-    this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
-  },
-};
-const eurowings = {
-  name: 'Eurowings',
-  iataCode: 'EW',
-  bookings: [],
-};
-const swiss = {
-  name: 'Swiss Airlines',
-  iataCode: 'LX',
-  bookings: [],
-};
+// const lufthansa = {
+//   name: 'Lufthansa',
+//   iataCode: 'LH',
+//   bookings: [],
+//   book(flightNum, name) {
+//     console.log(
+//       `${name} booked a seat on ${this.name} flight ${this.iataCode}${flightNum}`
+//     );
+//     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
+//   },
+// };
+// const eurowings = {
+//   name: 'Eurowings',
+//   iataCode: 'EW',
+//   bookings: [],
+// };
+// const swiss = {
+//   name: 'Swiss Airlines',
+//   iataCode: 'LX',
+//   bookings: [],
+// };
 
-// Call a function from another object to the object you want to
-const book = lufthansa.book;
-book.call(eurowings, 23, 'Fernando Valerio');
-book.call(lufthansa, 43, 'Alex Serafini');
-book.call(swiss, 54, 'David Frias');
+// // Call a function from another object to the object you want to
+// const book = lufthansa.book;
+// // book.call(eurowings, 23, 'Fernando Valerio');
+// // book.call(lufthansa, 43, 'Alex Serafini');
+// // book.call(swiss, 54, 'David Frias');
 
-// The apply method, the only difference is that you need to use an array of parameters
-const flightData = [43, 'Fernando Valerio'];
-book.apply(swiss, flightData);
+// // The apply method, the only difference is that you need to use an array of parameters
+// const flightData = [43, 'Fernando Valerio'];
+// // book.apply(swiss, flightData);
+
+// // Bind method
+// const bookEW = book.bind(eurowings);
+// bookEW(23, 'Steven Williams');
+
+// // Creating functions with set variables with bind (PARTIAL APLICATION)
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Fernando Valerio');
+
+// // With event listeners
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function () {
+//   this.planes++;
+//   console.log(this);
+//   console.log(this.planes);
+// };
+
+// document
+//   .querySelector('.buy')
+//   .addEventListener('click', lufthansa.buyPlane.bind(lufthansa));
+
+// // // Partial Aplication
+
+// // const addTax = (rate, value) => value + value * rate;
+
+// // // First parameter set to default because you dont need this. in this function
+// // const addVAT = addTax.bind(null, 0.23);
+// // console.log(addVAT(2400));
+
+// const addTax = rate => {
+//   return value => {
+//     console.log(value + value * rate);
+//   };
+// };
+
+// addTax(0.23)(2400);
+
+// - Coding Challenge 1
+
+// 3. Create a method 'displayResults' which displays the poll results. The
+// method takes a string as an input (called 'type'), which can be either 'string'
+// or 'array'. If type is 'array', simply display the results array as it is, using
+// console.log(). This should be the default option. If type is 'string', display a
+// string like "Poll results are 13, 2, 4, 1".
+// 4. Run the 'displayResults' method at the end of each
+// 'registerNewAnswer' method call.
+// 5. Bonus: Use the 'displayResults' method to display the 2 arrays in the test
+// data. Use both the 'array' and the 'string' option. Do not put the arrays in the poll
+// object! So what should the this keyword look like in this situation?
+
+// const poll = {
+//   question: 'What is your favourite programming language?',
+//   options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
+//   // This generates [0, 0, 0, 0]. More in the next section!
+//   answers: new Array(4).fill(0),
+//   registerNewAnswer() {
+//     const answer = Number(
+//       prompt(
+//         `${this.question}\n${this.options.join('\n')} \n(Write option number)`
+//       )
+//     );
+
+//     typeof answer === 'number' && answer < this.options.length
+//       ? this.answers[answer]++
+//       : alert('Not permitted input, try again');
+
+//     this.displayResults();
+//   },
+//   displayResults(type) {
+//     type === 'array'
+//       ? console.log(this.answers)
+//       : console.log(`Poll results are ${this.answers.join(' ')}`);
+//   },
+// };
+
+// document
+//   .querySelector('.poll')
+//   .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+// - IIFE
+
+// (function () {
+//   console.log('This will never run again');
+// })();
+
+// (() => {
+//   console.log('This will also never run again');
+// })();
+
+// - Clousures
+
+// const secureBooking = () => {
+//   let passengerCount = 0;
+
+//   return function () {
+//     passengerCount++;
+//     console.log(`${passengerCount} passengers`);
+//   };
+// };
+
+// const booker = secureBooking();
+// booker();
+// booker();
+// booker();
+
+// console.dir(booker);
+
+// Coding Challenge #2
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  document.body.addEventListener('click', () => {
+    header.style.color = 'blue';
+  });
+})();
--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -61,6 +61,44 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+const displayMovements = movements => {
+  containerMovements.innerHTML = '';
+  movements.forEach((mov, i) => {
+    const typeOfTransf = mov < 0 ? 'withdrawal' : 'deposit';
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${typeOfTransf}">${
+      i + 1
+    } ${typeOfTransf} </div>
+      <div class="movements__value">${mov}</div>
+    </div>`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+  });
+};
+
+const generateUsernames = accounts => {
+  accounts.map(acc => {
+    acc.username = acc.owner
+      .toLowerCase()
+      .split(' ')
+      .map(name => name[0])
+      .join('');
+  });
+};
+
+const calcDispBalance = movements => {
+  const balance = movements.reduce((acc, curr, i, arr) => {
+    return acc + curr;
+  }, 0);
+  labelBalance.textContent = `${balance} EUR`;
+};
+
+displayMovements(account1.movements);
+generateUsernames(accounts);
+calcDispBalance(account1.movements);
+console.log(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -71,6 +109,158 @@ const currencies = new Map([
   ['GBP', 'Pound sterling'],
 ]);
 
+/////////////////////////////////////////////////
+
+// let arr = ['a', 'b', 'c', 'd', 'e'];
+
+// - Array Slice Method not mutate
+// console.log(arr.slice(2));
+// console.log(arr.slice(2, 4));
+// console.log(arr.slice(-2));
+// console.log(arr.slice(-1));
+// console.log(arr.slice(1, -2));
+// console.log(arr.slice());
+
+// - Array Splice Method mutates
+// // console.log(arr.splice(2));
+// console.log(arr.splice(-1));
+// arr.splice(1, 2);
+// console.log(arr);
+
+// - Array Reverse Method mutates
+// arr = ['a', 'b', 'c', 'd', 'e'];
+// const arr2 = ['j', 'i', 'h', 'g', 'f'];
+// console.log(arr2.reverse());
+
+// - Array Concat Method not mutate
+// const letters = arr.concat(arr2);
+// console.log(letters);
+
+// - Array Join Method
+// console.log(letters.join(' - '));
+
+// - At Method
+// const arr = [23, 11, 64];
+// console.log(arr.at(0));
+// console.log(arr.at(-1));
+
+// -  Foreach in array
+// ? Conventional Way
+// for (const movement of movements) {
+//   if (movement > 0) {
+//     console.log(`You deposited ${movement}`);
+//   } else {
+//     console.log(`You withdrew ${movement}`);
+//   }
+// }
+
+// ? With forEach
+// movements.forEach((mov, i) => {
+//   if (mov > 0) {
+//     console.log(`Movement: ${i + 1} \nYou deposited ${mov}`);
+//   } else {
+//     console.log(`Movement: ${i + 1} \nYou withdrew ${mov}`);
+//   }
+// });
+
+// - Maps forEach
+// currencies.forEach((value, key, map) => {
+//   console.log(`${key}: ${value}`);
+// });
+
+// - Sets forEach
+// const currenciesUnique = new Set(['USD', 'GBP', 'USD', 'EUR', 'EUR']);
+// currenciesUnique.forEach((value, _, map) => {
+//   console.log(`${value}: ${value}`);
+// });
+
+// Coding Challenge #1
+
+// Create a function 'checkDogs', which accepts 2 arrays of dog's ages
+// ('dogsJulia' and 'dogsKate'), and does the following things:
+
+// 1. Julia found out that the owners of the first and the last two dogs actually have
+// cats, not dogs! So create a shallow copy of Julia's array, and remove the cat
+// ages from that copied array (because it's a bad practice to mutate function
+// parameters)
+
+// 2. Create an array with both Julia's (corrected) and Kate's data
+
+// 3. For each remaining dog, log to the console whether it's an adult ("Dog number 1
+// is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy
+// �
+// ")
+
+// 4. Run the function for both test datasets
+// Data 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+
+// const checkDogs = (dogsJulia, dogsKate) => {
+//   const copy = dogsJulia.slice(1, -2);
+//   const allDogs = copy.concat(dogsKate);
+//   allDogs.forEach((dog, i) => {
+//     console.log(
+//       dog < 3
+//         ? `Dog number ${i + 1} is still a puppy`
+//         : `Dog number ${i + 1} is an adult, and is ${dog} years old`
+//     );
+//   });
+// };
+
+// checkDogs([3, 5, 2, 12, 7], [4, 1, 15, 8, 3]);
+
+// const toEur = movements.map(item => item * 0.95);
+
+// console.log(toEur);
+
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-/////////////////////////////////////////////////
+// const deposits = movements.filter((mov, i) => {
+//   return mov > 0;
+// });
+
+// const withdrawal = movements.filter(mov => mov < 0);
+
+// const balance = movements.reduce((acc, cur, i, arr) => {
+//   return acc + cur;
+// }, 0);
+
+// console.log(deposits);
+// console.log(withdrawal);
+// console.log(balance);
+
+// const maximumBalance = movements.reduce((acc, curr) => {
+//   return acc > curr ? acc : (acc = curr);
+// }, movements[0]);
+
+// console.log(maximumBalance);
+
+// Coding Challenge 2
+
+// Create a function 'calcAverageHumanAge', which accepts an arrays of dog's
+// ages ('ages'), and does the following things in order:
+
+// const calcAverageHumanAge = ages => {
+//   const agesArray = ages.map((dog, i) => {
+//     return dog <= 2 ? 2 * dog : 16 + dog * 4;
+//   });
+
+//   const filter = agesArray.filter(item => {
+//     return item >= 18;
+//   });
+
+//   return filter;
+// };
+
+// console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
+
+// 3. Calculate the average human age of all adult dogs (you should already know
+// from other challenges how we calculate averages �)
+
+// 4. Run the function for both test datasets
+
+// PIPELINE
+const totalDeposits = movements
+  .filter(mov => mov > 0)
+  .map(mov => mov * 1.1)
+  .reduce((acc, mov) => acc + mov);
+console.log(totalDeposits);
